4d303f3dc8963c62f57dcc7bfb91afd109405ba2
vsspasova13 <vsspasova@abv.bg>
2024-12-13 00:30:42 +0200

    sem4

--- Haskell/ex/sem04.hs
+++ Haskell/ex/sem04.hs
@@ -1,5 +1,5 @@
 import Data.List (nub, maximumBy, minimumBy)
-import Data.Ord(comparing)
+import Data.Ord  (comparing)
 
 find :: Eq a => [(a,b)] -> a -> b
 find [] key = error "xs does not contains key"
@@ -66,4 +66,56 @@ failingStudents records = multipleFails
           stFailed = map students failing
           multipleFails = filter (\st -> length (filter (==st) stFailed) > 1) (nub stFailed)
 
--- to do graphs
+type Node = Int
+type Edge = (Node, Node)
+type AdjNode = (Node, [Node])
+type Graph = [AdjNode]
+
+nodes :: Graph -> [Node]
+nodes = map fst
+
+neighbors :: Graph -> Node -> [Node]
+neighbors graph node = head [ns | (n,ns) <- graph, n == node]
+
+dfs :: Graph -> Node -> [Node] -> [Node]
+dfs graph node visited
+    |node `elem` visited = visited
+    |otherwise = foldr (dfs graph) (node: visited) (neighbors graph node)
+
+buildGraph :: [Node] -> [Edge] -> Graph
+buildGraph nodes edges = [(n, neighbors n) | n <- nodes]
+    where 
+        neighbors node = [y | (x,y) <- edges, x == node] ++ [x | (x,y) <- edges, y == node]
+
+reachableNodes :: Graph -> Node -> [Node]
+reachableNodes graph node = dfs graph node []
+
+connectedComponents :: Graph -> [[Node]]
+connectedComponents graph = helper (nodes graph) [] []
+    where helper [] _ ccs = ccs
+          helper (n:ns) visited ccs
+            |n `elem` visited = helper ns visited ccs
+            |otherwise = helper ns (visited ++ cc) (cc : ccs)
+            where 
+                cc = reachableNodes graph n
+
+isFullyConnected :: Graph -> Bool
+isFullyConnected graph = length (connectedComponents graph) == 1
+
+bfs :: Graph -> Node -> Node -> Int
+bfs graph start end = bfsHelper [(start, 0)] []
+    where 
+      neighbors n = case lookup n graph of 
+        Just ns -> ns
+        Nothing -> []
+      bfsHelper [] _ = maxBound
+      bfsHelper ((n,d):queue) visited
+        |n == end = d
+        |n `elem` visited = bfsHelper queue visited
+        |otherwise = bfsHelper (queue ++ [(neighbor, d + 1) | neighbor <- neighbors n]) (n:visited)
+
+diameter :: Graph -> Int
+diameter graph = maximum [bfs graph a b | (a,_) <- graph, (b, _) <- graph, a /= b]
+
+nodesAtDistance :: Graph -> Int -> [(Node, Node)]
+nodesAtDistance graph k = [(a,b) | (a,_) <- graph, (b, _) <- graph, a<b, bfs graph a b == k]
